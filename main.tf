71dc8c7638b8ada29e53e1a52fcff896a896473b
Francisco Villegas <fcovillegast@gmail.com>
2025-11-06 13:07:43 -0300

    main test

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "subnets" {
-  source = "git::https://github.com/fcovillegast/TERRAFORM_AWS_SUBNET.git?ref=v1.1.1"
-  count = var.cidr_list
+  source = "git::https://github.com/fcovillegast/TERRAFORM_AWS_SUBNET.git?ref=main"
+  count = length(var.cidr_list)
 
   vpc_id = var.vpc_id
   name_prefix = var.name_prefix
   availability_zone = var.availability_zone
   tags = var.tags
+  cidr_block = var.cidr_list[count.index]
 }
